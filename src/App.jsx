9504f5adffa9d9cf5ff585bfc04edf53e8273ba3
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-02-07 12:59:29 +0100

    first commit: project creation

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main>
+      <div>
+        <ol>
+          <li>
+            <span>Player 1</span>
+            <span>X</span>
+          </li>
+
+          <li>
+            <span>Player 2</span>
+            <span>0</span></li>
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App
